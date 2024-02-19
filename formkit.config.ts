4000376055a8ca8ebcc081f827950016ab078514
Sirkastik <riobavictor2019@gmail.com>
2024-02-20 01:15:53 +0300

    feat: remove formkit

--- formkit.config.ts
+++
@@ -1,9 +0,0 @@
-// formkit.config.ts
-import { defaultConfig } from '@formkit/vue'
-import { rootClasses } from './formkit.theme'
-
-export default defaultConfig({
-  config: {
-    rootClasses,
-  },
-})
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -46,12 +46,6 @@ export default defineNuxtConfig({
     "@nuxt/ui",
     "@nuxt/image",
     "@nuxtjs/kinde",
-    [
-      "@formkit/nuxt",
-      {
-        autoImport: true,
-      },
-    ],
   ],
 
   experimental: {
